5628038f0d6869215e84e814156cb25cec7220a8
vladyslavnUA <vladyslavn@yahoo.com>
2021-02-08 18:10:43 -0500

    prototype complete

--- app/views.py
+++ app/views.py
@@ -1,32 +1,46 @@
-import arithmaticpy
-from arithmaticpy import vlad
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from .models import *
 
 @csrf_exempt
 def index(request):
     context = {}
-    # if request.method == 'POST':
+
     tocalc = request.POST.get('whatis')
-    allans = []
 
     if ('+' in str(tocalc)):
         ans = int(tocalc.split('+')[0]) + int(tocalc.split('+')[1])
-        final = str(tocalc), " = ", str(ans)
+        finale = str(tocalc), " = ", str(ans)
         context['ans'] = ans
-        context['final'] = final
+        tb = Answers.objects.create(final=finale)
+        tb.save()
         context['tocalc'] = tocalc
+        # context['tb'] = tb
+
     elif ('-' in str(tocalc)):
         ans = int(tocalc.split('-')[0]) - int(tocalc.split('-')[1])
-        context['ans'] = ans
-        context['tocalc'] = tocalc
-    elif ('*' in str(tocalc)):
-        ans = int(tocalc.split('*')[0]) * int(tocalc.split('*')[1])
-        context['ans'] = ans
-        context['tocalc'] = tocalc
-    elif ('/' in str(tocalc)):
-        ans = int(tocalc.split('/')[0]) / int(tocalc.split('/')[1])
+        finale = str(tocalc), " = ", str(ans)
+        tb = Answers.objects.create(final=finale)
+        tb.save()
         context['ans'] = ans
         context['tocalc'] = tocalc
 
+    elif ('*' in str(tocalc)):
+        ans = int(tocalc.split('*')[0]) * int(tocalc.split('*')[1])
+        finale = str(tocalc), " = ", str(ans)
+        tb = Answers.objects.create(final=finale)
+        tb.save()
+        context['ans'] = ans
+        context['tocalc'] = tocalc
+
+    elif ('/' in str(tocalc)):
+        ans = int(tocalc.split('/')[0]) / int(tocalc.split('/')[1])
+        finale = str(tocalc), " = ", str(ans)
+        tb = Answers.objects.create(final=finale)
+        tb.save()
+        context['ans'] = ans
+        context['tocalc'] = tocalc
+
+    last = Answers.objects.all().order_by('-final')[:10]
+    context['last'] = last
     return render(request, 'app/index.html', context)
